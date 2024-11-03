1c5f69f7dbb6b3908f0a3c472bb7466326616197
AntwortEinesLebens <AntwortEinesLebens@proton.me>
2024-11-03 17:27:22 +0100

    feat: ✨ Remove the banner

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,7 @@ use cli::{Arguments, Commands};
 use std::error::Error;
 use traces::Runnable;
 
-fn banner() {
-    let banner: &str = "
-
-    ██     ██  █████   ██████  
-    ██     ██ ██   ██ ██       
-    ██  █  ██ ███████ ██   ███ 
-    ██ ███ ██ ██   ██ ██    ██ 
-     ███ ███  ██   ██  ██████ 
-    
-    ";
-    println!("{}", banner);
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
-    banner();
-
     match Arguments::parse().command {
         Commands::Traces(action) => action.run()?,
     };
